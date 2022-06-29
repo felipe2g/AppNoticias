3f60a194c6a47ab87788f0855fabf5d578526c87
Felipe Guimarães <54296382+felipe2g@users.noreply.github.com>
2022-06-29 15:48:30 -0300

    change name of data var in the main view controller and remove loadNews function who load info of json in past

--- AppNoticias/Controller/MainTableViewController.swift
+++ AppNoticias/Controller/MainTableViewController.swift
@@ -9,42 +9,28 @@ import UIKit
 
 class MainTableViewController: UITableViewController {
     
-    var news: [ResultNews] = []
+    var resultNews: [ResultNews] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadNews()
-    }
-    
-    func loadNews() {
-        let fileUrl = Bundle.main.url(forResource: "file", withExtension: "json")!
-        let jsonData = try! Data(contentsOf: fileUrl)
-        
-        do {
-            let data = try JSONDecoder().decode(ResponseNews.self, from: jsonData)
-            
-            news = data.results
-        } catch {
-            print("error ==> \(String(describing: error))")
-        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print("count => \(news.count)")
-        return news.count
+        print("count => \(resultNews.count)")
+        return resultNews.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! NewYorkTableViewCell
         
-        let newsData = news[indexPath.row]
+        let newsData = resultNews[indexPath.row]
         cell.prepare(with: newsData)
         
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if let url = URL(string: news[indexPath.row].url) {
+        if let url = URL(string: resultNews[indexPath.row].url) {
             UIApplication.shared.open(url)
         }
     }
