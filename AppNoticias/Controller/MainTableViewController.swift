7814a28b76edc599496ce8ff52bedf3ced20e92e
Felipe Guimarães <54296382+felipe2g@users.noreply.github.com>
2022-06-29 16:12:15 -0300

    add news table view to get images from service

--- AppNoticias/Controller/MainTableViewController.swift
+++ AppNoticias/Controller/MainTableViewController.swift
@@ -9,10 +9,30 @@ import UIKit
 
 class MainTableViewController: UITableViewController {
     
-    var resultNews: [ResultNews] = []
+    var resultNews: [NewsData] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        NetworkManager.shared.getNews{ [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case .success(let response):
+                for item in response {
+                    if let imageURL = item.media.first?.mediaMetadata.last?.url {
+                        let data = NewsData(title: item.title, byline: item.byline, image: imageURL, url: item.url)
+                        self.resultNews.append(data)
+                    }
+                }
+                // É importante que o reload aconteça na main thread pois o reloadData recarrega o UI
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            case .failure(let error):
+                print("error: \(error)")
+            }
+        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
